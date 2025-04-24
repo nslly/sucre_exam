acf5f7b8008133f9190489c633a41486d3b4439c
ENS <50107631+nslly@users.noreply.github.com>
2025-04-24 23:53:57 +0800

    Update GenderIdentification.php

--- app/Enums/GenderIdentification.php
+++ app/Enums/GenderIdentification.php
@@ -6,14 +6,6 @@ enum GenderIdentification: int
     case MALE = 1;
     case FEMALE = 2;
 
-
-    public function label(): string
-    {
-        return match($this) {
-            self::Male => 'Male',
-            self::Female => 'Female',
-        };
-    }
 }
 
 
